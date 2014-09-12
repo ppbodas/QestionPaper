59b25b491ba60aa67a40107b99f43bb23c0c049f
Prathmesh Bodas <prathmesh@gmail.com>
2014-09-12 20:47:04 +0530

    Changing to Postgrace DB

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,32 @@
 
 ActiveRecord::Schema.define(version: 20140906144517) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
+    t.integer  "order_id"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "paper_items", force: true do |t|
     t.integer  "paper_id"
     t.integer  "question_id"
@@ -21,6 +47,15 @@ ActiveRecord::Schema.define(version: 20140906144517) do
   end
 
   create_table "papers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
