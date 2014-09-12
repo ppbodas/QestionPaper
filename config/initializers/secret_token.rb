b14d2b7b742513d0db1a674f16130efc907c5cb8
Prathmesh Bodas <prathmesh@gmail.com>
2014-09-12 20:56:36 +0530

    Removing secret token from git ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QestionPaper::Application.config.secret_key_base = '24ed3fe16bee0edbf53d7480173f1cb8cf11ed799b989ffb5bc3a2e05b982919e774cbe434af5525c2cccfafe8a2e8a004925ebbfc16b99674bd89a616b5e594'
